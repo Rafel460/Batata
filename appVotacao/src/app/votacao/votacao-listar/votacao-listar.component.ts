fd406dc4a32bc219f9f5b2be96150111feec5b7f
Rafel460 <rafaelmonte46@outlook.com>
2019-09-04 15:44:35 -0300

    versao 04/10

--- appVotacao/src/app/votacao/votacao-listar/votacao-listar.component.ts
+++ appVotacao/src/app/votacao/votacao-listar/votacao-listar.component.ts
@@ -4,13 +4,17 @@ import {Votacao} from '../entidade/votacao';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import * as _ from 'lodash';
 @Component({
   selector: 'app-votacao-listar',
   templateUrl: './votacao-listar.component.html',
   styleUrls: ['./votacao-listar.component.scss'],
 })
 export class VotacaoListarComponent implements OnInit {
-
+  valor : string;
+  listarFiltro : Votacao[];
+  filtro  = {};
+  votacoes : any;
   listadevotacoes : Observable<Votacao[]>;
   constructor(private banco : AngularFireDatabase, private router : Router) {
     this.listadevotacoes = this.banco.list<Votacao>('votacao').snapshotChanges().pipe(
@@ -20,7 +24,18 @@ export class VotacaoListarComponent implements OnInit {
     );
    }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.listadevotacoes.subscribe(
+      votacao => {
+        this.votacoes = votacao;
+        this.listarFiltro = _.filter(this.votacoes, _.conforms(this.filtro));
+      }
+    )
+  }
+  filtrar(){
+    this.filtro['nome'] = val => val.includes(this.valor);
+    this.listarFiltro = _.filter(this.votacoes, _.conforms(this.filtro));
+  }
   criarVotacao(){
     this.router.navigate(['criar_votacao']);
   }
