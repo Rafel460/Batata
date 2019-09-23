a9a1e1f3e4f9677d8c16e0d4872bbdde41eae187
Rafel460 <rafaelmonte46@outlook.com>
2019-09-23 11:44:35 -0300

    Versao 23/09

--- appVotacao/src/app/votacao/votacao-listar/votacao-listar.component.ts
+++ appVotacao/src/app/votacao/votacao-listar/votacao-listar.component.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Router } from '@angular/router';
 import * as _ from 'lodash';
+import {AlertController} from '@ionic/angular';
 import { MenuController} from '@ionic/angular';
 @Component({
   selector: 'app-votacao-listar',
@@ -17,7 +18,9 @@ export class VotacaoListarComponent implements OnInit {
   filtro  = {};
   votacoes : any;
   listadevotacoes : Observable<Votacao[]>;
-  constructor(private banco : AngularFireDatabase, private router : Router, private menu : MenuController) {
+  votacao : Votacao = new Votacao();
+
+  constructor(private banco : AngularFireDatabase, private router : Router, private menu : MenuController, private alerta : AlertController) {
     this.listadevotacoes = this.banco.list<Votacao>('votacao').snapshotChanges().pipe(
       map( lista => lista.map( linha => ({
         key : linha.payload.key, ... linha.payload.val()
@@ -25,6 +28,19 @@ export class VotacaoListarComponent implements OnInit {
     );
    }
 
+
+   async mostrarAlerta(votacao){
+     const alert = await this.alerta.create({
+       header : votacao.nome,
+       subHeader : 'selecione uma das opções',
+      buttons : [votacao.opcao_1, votacao.opcao_2, votacao.opcao_3],
+     });
+     await alert.present();
+   }
+
+   abrirMenu(){
+     this.menu.open('start');
+   }
   ngOnInit() {
     this.listadevotacoes.subscribe(
       votacao => {
@@ -33,7 +49,6 @@ export class VotacaoListarComponent implements OnInit {
       }
     )
   }
-
   filtrar(){
     this.filtro['nome'] = val => val.includes(this.valor);
     this.listarFiltro = _.filter(this.votacoes, _.conforms(this.filtro));
